888ea07dbec0cfd00d5a2c37a7b5299c60a0156e
GabrielFig <gabriggel2000@gmail.com>
2021-06-26 16:55:16 -0600

    add options to select picker of semestre and horario field

--- crudEscuela/crudapp/models.py
+++ crudEscuela/crudapp/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from django.utils.regex_helper import Choice
 
 class Admin(models.Model):
     nombre = models.CharField(max_length=50, default='DEFAULT VALUE')
     apellido_paterno = models.CharField(max_length=30, default='DEFAULT VALUE')
     apellido_materno = models.CharField(max_length=30, default='DEFAULT VALUE')
     puesto = models.CharField(max_length=50, default='DEFAULT VALUE')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
          db_table = 'admin'
+
+SEMESTRES = (('1', '1'),
+               ('2', '2'),
+               ('3', '3'),
+               ('4', '4'),
+               ('5', '5'),
+               ('6', '6'),
+               ('7', '7'),
+               ('8', '8'),
+               ('9', '9'),
+               )
 
 
 class Alumnos(models.Model):
@@ -19,9 +29,9 @@ class Alumnos(models.Model):
     fecha_nac = models.DateField()
     direccion = models.CharField(max_length=30)
     num_cel = models.CharField(max_length=16)
-    semestre = models.IntegerField()
+    semestre = models.CharField(max_length=2, choices=SEMESTRES)
     correo = models.EmailField()
-    horario_alumno = models.CharField(max_length=30)
+    horario_alumno = models.CharField(max_length=30, choices=(('1','Matutino'), ('2','Vespertino')))
 
     class Meta:
         db_table = 'alumnos'
@@ -32,7 +42,6 @@ class Maestro(models.Model):
     apellido_paterno = models.CharField(max_length=30)
     apellido_materno = models.CharField(max_length=30)
     asignatura = models.CharField(max_length=20)
-    img = models.FileField()
     horario = models.CharField(max_length=20)
 
     class Meta:
